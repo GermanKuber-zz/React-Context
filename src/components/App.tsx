1d3ec1733c030773e3a5768c97538ae30534fe1b
Germán <german.kuber@outlook.com>
2019-10-17 14:39:31 -0300

    Witchout redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, MouseEvent } from "react";
 import "../styles/index.css";
 import Login from "./Login";
 import { Router, Route, Switch } from "react-router-dom";
@@ -17,40 +17,56 @@ import { PrivateRoute } from "./router/PrivateRoute";
 import EditEvent from "./Admin/EditEvent/Index";
 import Events from "./Admin/Events/Index";
 import SyncEvent from "./Admin/SyncEvent/Index";
+import { css } from "@emotion/core";
+import LoadingOverlay from "react-loading-overlay";
+
 export const App: React.SFC = () => {
+  const override = css`
+    display: block;
+    width: 50%;
+    left: 50%;
+    top: 50%;
+  `;
   useEffect(() => {}, []);
+  const [loading, setLoading] = useState(false);
+
+  const handleCancelEvent = (event: MouseEvent<HTMLButtonElement>) => {
+    setLoading(!loading);
+  };
   return (
     <>
       <Router history={historyRouter}>
         <Header></Header>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/organizers" component={Organizers} />
-            <Route path="/organizers/:id(\d+)?" component={OrganizerDetail} />
-            <Route path="/speaker/:id(\d+)?" component={SpeakerDetail} />
-            <Route path="/sponsor/:id(\d+)?" component={Sponsor} />
-            <PrivateRoute
-              exact
-              path="/admin/eventsToSync"
-              component={EventsToSync}
-            />
-            <PrivateRoute exact path="/admin/events" component={Events} />
-            <PrivateRoute
-              exact
-              path="/admin/events/:id(\d+)?/edit"
-              component={EditEvent}
-            />
-            <PrivateRoute
-              exact
-              path="/admin/eventsToSync/:id/:platform/sync"
-              component={SyncEvent}
-            />
-            <Route path="/login" component={Login} />
-            <PrivateRoute path="/panel" component={ControlPanel} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
+        <LoadingOverlay active={true} spinner text="Procesando...">
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/organizers" component={Organizers} />
+              <Route path="/organizers/:id(\d+)?" component={OrganizerDetail} />
+              <Route path="/speaker/:id(\d+)?" component={SpeakerDetail} />
+              <Route path="/sponsor/:id(\d+)?" component={Sponsor} />
+              <PrivateRoute
+                exact
+                path="/admin/eventsToSync"
+                component={EventsToSync}
+              />
+              <PrivateRoute exact path="/admin/events" component={Events} />
+              <PrivateRoute
+                exact
+                path="/admin/events/:id(\d+)?/edit"
+                component={EditEvent}
+              />
+              <PrivateRoute
+                exact
+                path="/admin/eventsToSync/:id/:platform/sync"
+                component={SyncEvent}
+              />
+              <Route path="/login" component={Login} />
+              <PrivateRoute path="/panel" component={ControlPanel} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </LoadingOverlay>
       </Router>
       <Footer></Footer>
     </>
