985d7de3728b2a48398225c5a487e276030fd4e4
Germán Küber <german.kuber@outlook.com>
2019-10-18 21:27:31 -0300

    Add qr scanner

--- src/components/Admin/Events/EventLive.tsx
+++ src/components/Admin/Events/EventLive.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, MouseEvent } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import QrReader from "react-qr-scanner";
 import { hasAny } from "../../../services/objectsservices";
@@ -15,17 +15,37 @@ export const EventLive: React.SFC<
 > = () => {
   const [attended, setAttended] = useState(new Array<string>());
   const [showReader, setShowReader] = useState(true);
-  const handleScan = (data: any) => {
+  const handleScan = (data: string) => {
     if (data) {
       setShowReader(false);
 
       setTimeout(() => {
         setShowReader(true);
       }, 1000);
-      var newArry = [...attended, data];
+      const emailOfUser = data.split("|");
+      var newArry = [...attended, emailOfUser[0]];
       setAttended(newArry);
+      localStorage.setItem("attendedList", JSON.stringify(newArry));
     }
   };
+  const handleDelete = (
+    event: MouseEvent<HTMLButtonElement>,
+    email: string
+  ) => {
+    event.preventDefault();
+    const positionToDelete = attended.indexOf(email);
+    const newArry = [
+      ...attended.slice(0, positionToDelete),
+      ...attended.slice(positionToDelete + 1, attended.length)
+    ];
+    setAttended(newArry);
+    localStorage.setItem("attendedList", JSON.stringify(newArry));
+  };
+
+  const handleSync = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   const handleError = (err: any) => {
     console.error(err);
   };
@@ -50,22 +70,39 @@ export const EventLive: React.SFC<
       </div>
 
       {hasAny(attended) && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {attended.map(x => {
-              <tr key={x}>
-                <th scope="row">1</th>
-                <td>Mark</td>
-              </tr>;
-            })}
-          </tbody>
-        </table>
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Email</th>
+                <th scope="col">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {attended.map(x => (
+                <tr key={x}>
+                  <td>{x}</td>
+                  <td>
+                    <button
+                      onClick={e => handleDelete(e, x)}
+                      type="button"
+                      className="btn btn-danger"
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button
+            onClick={e => handleSync(e)}
+            type="button"
+            className="btn btn-success"
+          >
+            Sincronizar
+          </button>
+        </>
       )}
     </>
   );
