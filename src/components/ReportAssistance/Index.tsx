2f1d7ecea3e6a6d19b4458fdfba1fa8d83ba7c57
Germán <german.kuber@outlook.com>
2019-10-20 18:00:48 -0300

    Add qr generator

--- src/components/ReportAssistance/Index.tsx
+++ src/components/ReportAssistance/Index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory, RouteComponentProps, Route } from "react-router";
 import { Link } from "react-router-dom";
+import QRCode from "qrcode.react";
 type ReportAssistanceProps = {};
 
 const ReportAssistance: React.SFC<
@@ -10,6 +11,7 @@ const ReportAssistance: React.SFC<
   return (
     <>
       <h1>Report Assistence</h1>
+      <QRCode value="asdasd90a8sd09as8d90asjdiasdja6sdta8sda!-asda.sdas" />
     </>
   );
 };
